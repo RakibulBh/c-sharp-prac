70d86e484e5c569b76d9fb3b55b71f292a7ef5fd
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-11-07 21:29:30 +0000

    Initial commit: Basic C# calculator program

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace first_program;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        while (true) {
+            Console.Write("Enter first number: ");
+            string num1 = Console.ReadLine() ?? "0";
+            Console.Write("Enter second number: ");
+            string num2 = Console.ReadLine() ?? "0";
+
+            Console.WriteLine(Int32.Parse(num1) + Int32.Parse(num2));
+        }
+    }
+}
